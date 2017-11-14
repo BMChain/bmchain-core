bc35bc5336561212a9add7a557a60af137db72f7
Igor <igor.soldatov@gmail.com>
2017-11-14 12:13:45 +0300

    New parameters for bmchain

--- libraries/protocol/include/steemit/protocol/config.hpp
+++ libraries/protocol/include/steemit/protocol/config.hpp
@@ -40,14 +40,14 @@
 #define STEEMIT_OWNER_AUTH_HISTORY_TRACKING_START_BLOCK_NUM 1
 #else // IS LIVE STEEM NETWORK
 
-#define STEEMIT_INIT_PUBLIC_KEY_STR             "STM76G5486bhLKXkXgN1nSkeDbpWEaiavF7i6tDULdpvyr69CLbHC"
+#define STEEMIT_INIT_PUBLIC_KEY_STR             "BMT76G5486bhLKXkXgN1nSkeDbpWEaiavF7i6tDULdpvyr69CLbHC"
 #define STEEMIT_CHAIN_ID                        (fc::sha256::hash("bmchain"))
 #define VESTS_SYMBOL  (uint64_t(6) | (uint64_t('V') << 8) | (uint64_t('E') << 16) | (uint64_t('S') << 24) | (uint64_t('T') << 32) | (uint64_t('S') << 40)) ///< VESTS with 6 digits of precision
-#define STEEM_SYMBOL  (uint64_t(3) | (uint64_t('S') << 8) | (uint64_t('T') << 16) | (uint64_t('E') << 24) | (uint64_t('E') << 32) | (uint64_t('M') << 40)) ///< STEEM with 3 digits of precision
+#define STEEM_SYMBOL  (uint64_t(3) | (uint64_t('B') << 8) | (uint64_t('M') << 16) | (uint64_t('T') << 24))  ///< STEEM with 3 digits of precision
 #define SBD_SYMBOL    (uint64_t(3) | (uint64_t('S') << 8) | (uint64_t('B') << 16) | (uint64_t('D') << 24) ) ///< STEEM Backed Dollars with 3 digits of precision
 #define STMD_SYMBOL   (uint64_t(3) | (uint64_t('S') << 8) | (uint64_t('T') << 16) | (uint64_t('M') << 24) | (uint64_t('D') << 32) ) ///< STEEM Dollars with 3 digits of precision
-#define STEEMIT_SYMBOL                          "STEEM"
-#define STEEMIT_ADDRESS_PREFIX                  "STM"
+#define STEEMIT_SYMBOL                          "BMT"
+#define STEEMIT_ADDRESS_PREFIX                  "BMT"
 
 #define STEEMIT_GENESIS_TIME                    (fc::time_point_sec(1458835200))
 #define STEEMIT_MINING_TIME                     (fc::time_point_sec(1458838800))
--- programs/steemd/main.cpp
+++ programs/steemd/main.cpp
@@ -57,6 +57,14 @@ int main(int argc, char** argv) {
       std::cerr << "blockchain version: " << fc::string( STEEMIT_BLOCKCHAIN_VERSION ) << "\n";
       std::cerr << "------------------------------------------------------\n";
 #else
+
+       auto priv_key = fc::ecc::private_key::regenerate(fc::sha256::hash(std::string("it does not do to dwell on dreams and forget to live")));
+       auto priv_key_wif = graphene::utilities::key_to_wif(priv_key);
+       auto publ_key = std::string( steemit::protocol::public_key_type(priv_key.get_public_key()));
+
+       std::cerr << "priv_key: " << priv_key_wif << std::endl;
+       std::cerr << "publ_key: " << publ_key << std::endl;
+
       std::cerr << "------------------------------------------------------\n\n";
       std::cerr << "            STARTING STEEM NETWORK\n\n";
       std::cerr << "------------------------------------------------------\n";
