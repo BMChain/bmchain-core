551dcf571c2a334ea549c79ba46315296e9cf9f1
igor <igor.soldatov@gmail.com>
2018-07-23 21:56:20 +0300

    threadsafe update

--- libraries/net/threadsafe.cpp
+++ libraries/net/threadsafe.cpp
@@ -19,16 +19,71 @@ private:
     node* tail;
     std::condition_variable data_cond;
 
+    node* get_tail(){
+        std::lock_guard<std::mutex> tail_lock(tail_mutex);
+        return tail;
+    }
+
+    std::unique_ptr<node> pop_head(){
+        std::unique_ptr<node> old_head = std::move(head);
+        head = std::move(old_head->next);
+        return old_head;
+    }
+
+    std::unique_lock<std::mutex> wait_for_data(){
+        std::unique_lock<std::mutex> head_lock(head_mutex);
+        data_cond.wait(head_lock, [&]{return head.get() != get_tail();});
+        return std::move(head_lock);
+    }
+
+    std::unique_ptr<node> wait_pop_head(){
+        std::unique_lock<std::mutex> head_lock(wait_for_data());
+        return pop_head();
+    }
+
+    std::unique_ptr<node> wait_pop_head(T& value){
+        std::unique_lock<std::mutex> head_lock(wait_for_data());
+        value = std::move(*head->data);
+        return pop_head();
+    }
+
+    std::unique_ptr<node> try_pop_head(){
+        std::lock_guard<std::mutex> head_lock(head_mutex);
+        if(head.get() == get_tail()){
+            return std::unique_ptr<node>();
+        }
+        return pop_head();
+    }
+
+    std::unique_ptr<node> try_pop_head(T& value){
+        std::lock_guard<std::mutex> head_lock(head_mutex);
+        if(head.get() == get_tail()){
+            return std::unique_ptr<node>();
+        }
+        value = std::move(*head->data);
+        return pop_head();
+    }
+
 public:
     threadsafe_queue(): head(new node), tail(head.get()){}
     threadsafe_queue(const threadsafe_queue& other)=delete;
     threadsafe_queue& operator=(const threadsafe_queue& other)=delete;
-    std::shared_ptr<T> try_pop();
-    bool try_pop(T& value);
+    std::shared_ptr<T> try_pop(){
+        std::unique_ptr<node> old_head = try_pop_head();
+        return old_head ? old_head->data : std::shared_ptr<T>();
+    }
+    bool try_pop(T& value){
+        std::unique_ptr<node> const old_head = try_pop_head(value);
+        //return old_head;
+        return true;
+    }
     std::shared_ptr<T> wait_and_pop();
     void wait_and_pop(T& value);
     void push(T new_value);
-    void empty();
+    void empty(){
+        std::lock_guard<std::mutex> head_lock(head_mutex);
+        return (head.get() == get_tail());
+    }
 };
 
 template <typename T>
