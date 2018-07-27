42deb74f0caf03bf8be8f2f9cf75cb68b38ed29d
igor <igor.soldatov@gmail.com>
2018-07-27 23:37:17 +0300

    lookup

--- libraries/net/threadsafe.cpp
+++ libraries/net/threadsafe.cpp
@@ -7,6 +7,9 @@
 #include <condition_variable>
 #include <exception>
 #include <stack>
+#include <list>
+#include "boost/thread/pthread/shared_mutex.hpp"
+
 
 namespace threadsafe {
 
@@ -160,5 +163,49 @@ namespace threadsafe {
         }
         data_cond.notify_one();
     }
+
+    template <typename Key, typename Value, typename Hash=std::hash<Key>>
+    class threadsafe_lookup_table{
+    private:
+        class bucket_type {
+        private:
+            typedef std::pair<Key, Value> bucket_value;
+            typedef std::list<bucket_value> bucket_data;
+            typedef typename bucket_data::iterator bucket_iterator;
+
+            bucket_data data;
+            mutable boost::shared_mutex mutex;
+
+            bucket_iterator find_entry_for(Key const& key) const{
+                return std::find_if(data.begin(), data.end(), [&](bucket_value const& item){return item.first == key;});
+            }
+
+        public:
+            Value value_for(Key const& key, Value const& default_value) const{
+                boost::shared_lock<boost::shared_mutex> lock(mutex);
+                bucket_iterator const found_entry = find_entry_for(key);
+                return (found_entry == data.end()) ? default_value : found_entry->second;
+            }
+            void add_or_update_mapping(Key const& key, Value const& value){
+                std::unique_lock<boost::shared_mutex> lock(mutex);
+                bucket_iterator const found_entry = find_entry_for(key);
+                if (found_entry == data.end()){
+                    data.push_back(bucket_value(key, value));
+                }
+                else{
+                    found_entry->second = value;
+                }
+            }
+            void remove_mapping(Key const& key){
+                std::unique_lock<boost::shared_mutex> lock(mutex);
+                bucket_iterator const found_entry = find_entry_for(key);
+                if (found_entry != data.end()){
+                    data.erase(found_entry);
+                }
+            }
+        };
+
+
+    };
 
 }
