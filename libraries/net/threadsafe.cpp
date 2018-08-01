60d7dd38c4990808094ccc34906e01c88429eff0
igor <igor.soldatov@gmail.com>
2018-08-01 22:26:17 +0300

    fixed theadsafe list

--- libraries/net/threadsafe.cpp
+++ libraries/net/threadsafe.cpp
@@ -285,6 +285,34 @@ namespace threadsafe {
             new_node->next = std::move(head.next);
             head.next = std::move(new_node);
         }
+
+        template<typename Function>
+        void for_each(Function f){
+            node* current = &head;
+            std::unique_lock<std::mutex> lk(head.m);
+            while (node* const next = current->next.get()){
+                std::unique_lock<std::mutex> next_lk(next->m);
+                lk.unlock();
+                f(*next->data);
+                current = next;
+                lk = std::move(next_lk);
+            }
+        }
+
+        template<typename Predicate>
+        std::shared_ptr<T> find_first_if(Predicate p){
+            node* current = &head;
+            std::unique_lock<std::mutex> lk(head.m);
+            while (node* const next = current->next.get()){
+                std::unique_lock<std::mutex> next_lk(next->m);
+                lk.unlock();
+                if(p(*next->data)){
+                    return next->data;
+                }
+                current = next;
+
+            }
+        }
     };
 
 }
