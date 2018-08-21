4e33a02d04b6d749b2f2e080e54b40c55809b310
igor <igor.soldatov@gmail.com>
2018-08-21 21:18:16 +0300

    fixed asset symbol for smt

--- libraries/protocol/include/bmchain/protocol/asset_symbol.hpp
+++ libraries/protocol/include/bmchain/protocol/asset_symbol.hpp
@@ -13,16 +13,16 @@
 #define STEEM_PRECISION_SBD   (3)
 #define STEEM_PRECISION_STEEM (3)
 #define STEEM_PRECISION_VESTS (6)
-#define STEEM_PRECISION_BMT   (3)
-#define STEEM_PRECISION_REP   (6)
+#define STEEM_PRECISION_BMT (3)
+#define STEEM_PRECISION_REP (6)
 
 // One's place is used for check digit, which means NAI 0-9 all have NAI data of 0 which is invalid
 // This space is safe to use because it would alwasys result in failure to convert from NAI
-#define STEEM_NAI_SBD   (1)
-#define STEEM_NAI_STEEM (2)
-#define STEEM_NAI_VESTS (3)
-#define STEEM_NAI_BMT   (4)
-#define STEEM_NAI_REP   (5)
+#define STEEM_NAI_BMT   (1)
+#define STEEM_NAI_REP   (2)
+#define STEEM_NAI_SBD   (3)
+#define STEEM_NAI_STEEM (4)
+#define STEEM_NAI_VESTS (5)
 
 #define STEEM_ASSET_NUM_SBD \
   (((SMT_MAX_NAI + STEEM_NAI_SBD)   << STEEM_ASSET_SYMBOL_PRECISION_BITS) | STEEM_PRECISION_SBD)
@@ -31,9 +31,9 @@
 #define STEEM_ASSET_NUM_VESTS \
   (((SMT_MAX_NAI + STEEM_NAI_VESTS) << STEEM_ASSET_SYMBOL_PRECISION_BITS) | STEEM_PRECISION_VESTS)
 #define STEEM_ASSET_NUM_BMT \
-  (((SMT_MAX_NAI + STEEM_NAI_BMT)   << STEEM_ASSET_SYMBOL_PRECISION_BITS) | STEEM_PRECISION_BMT)
+  (((SMT_MAX_NAI + STEEM_NAI_BMT) << STEEM_ASSET_SYMBOL_PRECISION_BITS) | STEEM_PRECISION_BMT)
 #define STEEM_ASSET_NUM_REP \
-  (((SMT_MAX_NAI + STEEM_NAI_REP)   << STEEM_ASSET_SYMBOL_PRECISION_BITS) | STEEM_PRECISION_REP)
+  (((SMT_MAX_NAI + STEEM_NAI_REP) << STEEM_ASSET_SYMBOL_PRECISION_BITS) | STEEM_PRECISION_REP)
 
 #ifdef IS_TEST_NET
 
@@ -55,7 +55,7 @@
 #define STEEM_SYMBOL_SER  (uint64_t(3) | (STEEM_SYMBOL_U64 << 8)) ///< STEEM|TESTS with 3 digits of precision
 #define SBD_SYMBOL_SER    (uint64_t(3) |   (SBD_SYMBOL_U64 << 8)) ///< SBD|TBD with 3 digits of precision
 #define BMT_SYMBOL_SER    (uint64_t(3) |   (BMT_SYMBOL_U64 << 8))
-#define REP_SYMBOL_SER    (uint64_t(3) |   (REP_SYMBOL_U64 << 8))
+#define REP_SYMBOL_SER    (uint64_t(6) |   (REP_SYMBOL_U64 << 8))
 
 #define STEEM_ASSET_MAX_DECIMALS 12
 
@@ -65,77 +65,77 @@
 
 namespace bmchain { namespace protocol {
 
-        class asset_symbol_type
-        {
-        public:
-            enum asset_symbol_space
-            {
-                legacy_space = 1,
-                smt_nai_space = 2
-            };
+      class asset_symbol_type
+      {
+      public:
+         enum asset_symbol_space
+         {
+            legacy_space = 1,
+            smt_nai_space = 2
+         };
 
-            asset_symbol_type() {}
+         asset_symbol_type() {}
 
-            // buf must have space for STEEM_ASSET_SYMBOL_MAX_LENGTH+1
-            static asset_symbol_type from_string( const std::string& str );
-            static asset_symbol_type from_nai_string( const char* buf, uint8_t decimal_places );
-            static asset_symbol_type from_asset_num( uint32_t asset_num )
-            {   asset_symbol_type result;   result.asset_num = asset_num;   return result;   }
-            static uint32_t asset_num_from_nai( uint32_t nai, uint8_t decimal_places );
-            static asset_symbol_type from_nai( uint32_t nai, uint8_t decimal_places )
-            {   return from_asset_num( asset_num_from_nai( nai, decimal_places ) );          }
+         // buf must have space for STEEM_ASSET_SYMBOL_MAX_LENGTH+1
+         static asset_symbol_type from_string( const std::string& str );
+         static asset_symbol_type from_nai_string( const char* buf, uint8_t decimal_places );
+         static asset_symbol_type from_asset_num( uint32_t asset_num )
+         {   asset_symbol_type result;   result.asset_num = asset_num;   return result;   }
+         static uint32_t asset_num_from_nai( uint32_t nai, uint8_t decimal_places );
+         static asset_symbol_type from_nai( uint32_t nai, uint8_t decimal_places )
+         {   return from_asset_num( asset_num_from_nai( nai, decimal_places ) );          }
 
-            std::string to_string()const;
+         std::string to_string()const;
 
-            void to_nai_string( char* buf )const;
-            std::string to_nai_string()const
-            {
-                char buf[ STEEM_ASSET_SYMBOL_NAI_STRING_LENGTH ];
-                to_nai_string( buf );
-                return std::string( buf );
-            }
+         void to_nai_string( char* buf )const;
+         std::string to_nai_string()const
+         {
+             char buf[ STEEM_ASSET_SYMBOL_NAI_STRING_LENGTH ];
+             to_nai_string( buf );
+             return std::string( buf );
+         }
 
-            uint32_t to_nai()const;
+         uint32_t to_nai()const;
 
-            /**Returns true when symbol represents vesting variant of the token,
-             * false for liquid one.
-             */
-            bool is_vesting() const;
-            /**Returns vesting symbol when called from liquid one
-             * and liquid symbol when called from vesting one.
-             * Returns back the SBD symbol if represents SBD.
-             */
-            asset_symbol_type get_paired_symbol() const;
-            /**Returns asset_num stripped of precision holding bits.
-             * \warning checking that it's SMT symbol is caller responsibility.
-             */
-            uint32_t get_stripped_precision_smt_num() const
-            {
-                return asset_num & ~( SMT_ASSET_NUM_PRECISION_MASK );
-            }
+         /**Returns true when symbol represents vesting variant of the token,
+          * false for liquid one.
+          */
+         bool is_vesting() const;
+         /**Returns vesting symbol when called from liquid one
+          * and liquid symbol when called from vesting one.
+          * Returns back the SBD symbol if represents SBD.
+          */
+         asset_symbol_type get_paired_symbol() const;
+         /**Returns asset_num stripped of precision holding bits.
+          * \warning checking that it's SMT symbol is caller responsibility.
+          */
+         uint32_t get_stripped_precision_smt_num() const
+         {
+             return asset_num & ~( SMT_ASSET_NUM_PRECISION_MASK );
+         }
 
-            asset_symbol_space space()const;
-            uint8_t decimals()const
-            {  return uint8_t( asset_num & 0x0F );    }
-            void validate()const;
+         asset_symbol_space space()const;
+         uint8_t decimals()const
+         {  return uint8_t( asset_num & 0x0F );    }
+         void validate()const;
 
-            friend bool operator == ( const asset_symbol_type& a, const asset_symbol_type& b )
-            {  return (a.asset_num == b.asset_num);   }
-            friend bool operator != ( const asset_symbol_type& a, const asset_symbol_type& b )
-            {  return (a.asset_num != b.asset_num);   }
-            friend bool operator <  ( const asset_symbol_type& a, const asset_symbol_type& b )
-            {  return (a.asset_num <  b.asset_num);   }
-            friend bool operator >  ( const asset_symbol_type& a, const asset_symbol_type& b )
-            {  return (a.asset_num >  b.asset_num);   }
-            friend bool operator <= ( const asset_symbol_type& a, const asset_symbol_type& b )
-            {  return (a.asset_num <= b.asset_num);   }
-            friend bool operator >= ( const asset_symbol_type& a, const asset_symbol_type& b )
-            {  return (a.asset_num >= b.asset_num);   }
+         friend bool operator == ( const asset_symbol_type& a, const asset_symbol_type& b )
+         {  return (a.asset_num == b.asset_num);   }
+         friend bool operator != ( const asset_symbol_type& a, const asset_symbol_type& b )
+         {  return (a.asset_num != b.asset_num);   }
+         friend bool operator <  ( const asset_symbol_type& a, const asset_symbol_type& b )
+         {  return (a.asset_num <  b.asset_num);   }
+         friend bool operator >  ( const asset_symbol_type& a, const asset_symbol_type& b )
+         {  return (a.asset_num >  b.asset_num);   }
+         friend bool operator <= ( const asset_symbol_type& a, const asset_symbol_type& b )
+         {  return (a.asset_num <= b.asset_num);   }
+         friend bool operator >= ( const asset_symbol_type& a, const asset_symbol_type& b )
+         {  return (a.asset_num >= b.asset_num);   }
 
-            uint32_t asset_num = 0;
-        };
+         uint32_t asset_num = 0;
+      };
 
-    } } // bmchain::protocol
+   } } // bmchain::protocol
 
 FC_REFLECT(bmchain::protocol::asset_symbol_type, (asset_num))
 
@@ -151,105 +151,105 @@ namespace fc { namespace raw {
 //
 // NAI internal storage of legacy assets
 
-        template< typename Stream >
-        inline void pack( Stream& s, const bmchain::protocol::asset_symbol_type& sym )
-        {
-            switch( sym.space() )
-            {
-                case bmchain::protocol::asset_symbol_type::legacy_space:
-                {
-                    uint64_t ser = 0;
-                    switch( sym.asset_num )
-                    {
-                        case STEEM_ASSET_NUM_STEEM:
-                            ser = STEEM_SYMBOL_SER;
-                            break;
-                        case STEEM_ASSET_NUM_SBD:
-                            ser = SBD_SYMBOL_SER;
-                            break;
-                        case STEEM_ASSET_NUM_VESTS:
-                            ser = VESTS_SYMBOL_SER;
-                            break;
-                        case STEEM_ASSET_NUM_BMT:
-                            ser = BMT_SYMBOL_SER;
-                            break;
-                        case STEEM_ASSET_NUM_REP:
-                            ser = REP_SYMBOL_SER;
-                            break;
-                        default:
-                            FC_ASSERT( false, "Cannot serialize unknown asset symbol" );
-                    }
-                    pack( s, ser );
-                    break;
-                }
-                case bmchain::protocol::asset_symbol_type::smt_nai_space:
-                    pack( s, sym.asset_num );
-                    break;
-                default:
-                    FC_ASSERT( false, "Cannot serialize unknown asset symbol" );
-            }
-        }
+      template< typename Stream >
+      inline void pack( Stream& s, const bmchain::protocol::asset_symbol_type& sym )
+      {
+          switch( sym.space() )
+          {
+              case bmchain::protocol::asset_symbol_type::legacy_space:
+              {
+                  uint64_t ser = 0;
+                  switch( sym.asset_num )
+                  {
+                      case STEEM_ASSET_NUM_STEEM:
+                          ser = STEEM_SYMBOL_SER;
+                        break;
+                      case STEEM_ASSET_NUM_SBD:
+                          ser = SBD_SYMBOL_SER;
+                        break;
+                      case STEEM_ASSET_NUM_VESTS:
+                          ser = VESTS_SYMBOL_SER;
+                        break;
+                      case STEEM_ASSET_NUM_BMT:
+                          ser = BMT_SYMBOL_SER;
+                        break;
+                      case STEEM_ASSET_NUM_REP:
+                          ser = REP_SYMBOL_SER;
+                        break;
+                      default:
+                          FC_ASSERT( false, "Cannot serialize unknown asset symbol" );
+                  }
+                  pack( s, ser );
+                  break;
+              }
+              case bmchain::protocol::asset_symbol_type::smt_nai_space:
+                  pack( s, sym.asset_num );
+                break;
+              default:
+                  FC_ASSERT( false, "Cannot serialize unknown asset symbol" );
+          }
+      }
 
-        template< typename Stream >
-        inline void unpack( Stream& s, bmchain::protocol::asset_symbol_type& sym )
-        {
-            uint64_t ser = 0;
-            s.read( (char*) &ser, 4 );
+      template< typename Stream >
+      inline void unpack( Stream& s, bmchain::protocol::asset_symbol_type& sym )
+      {
+          uint64_t ser = 0;
+          s.read( (char*) &ser, 4 );
 
-            switch( ser )
-            {
-                case STEEM_SYMBOL_SER & 0xFFFFFFFF:
-                    s.read( ((char*) &ser)+4, 4 );
-                    FC_ASSERT( ser == STEEM_SYMBOL_SER, "invalid asset bits" );
-                    sym.asset_num = STEEM_ASSET_NUM_STEEM;
-                    break;
-                case SBD_SYMBOL_SER & 0xFFFFFFFF:
-                    s.read( ((char*) &ser)+4, 4 );
-                    FC_ASSERT( ser == SBD_SYMBOL_SER, "invalid asset bits" );
-                    sym.asset_num = STEEM_ASSET_NUM_SBD;
-                    break;
-                case VESTS_SYMBOL_SER & 0xFFFFFFFF:
-                    s.read( ((char*) &ser)+4, 4 );
-                    FC_ASSERT( ser == VESTS_SYMBOL_SER, "invalid asset bits" );
-                    sym.asset_num = STEEM_ASSET_NUM_VESTS;
-                    break;
-                case BMT_SYMBOL_SER & 0xFFFFFFFF:
-                    s.read( ((char*) &ser)+4, 4 );
-                    FC_ASSERT( ser == BMT_SYMBOL_SER, "invalid asset bits" );
-                    sym.asset_num = STEEM_ASSET_NUM_BMT;
-                    break;
-                case REP_SYMBOL_SER & 0xFFFFFFFF:
-                    s.read( ((char*) &ser)+4, 4 );
-                    FC_ASSERT( ser == REP_SYMBOL_SER, "invalid asset bits" );
-                    sym.asset_num = STEEM_ASSET_NUM_REP;
-                    break;
-                default:
-                    sym.asset_num = uint32_t( ser );
-            }
-            sym.validate();
-        }
+          switch( ser )
+          {
+              case STEEM_SYMBOL_SER & 0xFFFFFFFF:
+                  s.read( ((char*) &ser)+4, 4 );
+                FC_ASSERT( ser == STEEM_SYMBOL_SER, "invalid asset bits" );
+                sym.asset_num = STEEM_ASSET_NUM_STEEM;
+                break;
+              case SBD_SYMBOL_SER & 0xFFFFFFFF:
+                  s.read( ((char*) &ser)+4, 4 );
+                FC_ASSERT( ser == SBD_SYMBOL_SER, "invalid asset bits" );
+                sym.asset_num = STEEM_ASSET_NUM_SBD;
+                break;
+              case VESTS_SYMBOL_SER & 0xFFFFFFFF:
+                  s.read( ((char*) &ser)+4, 4 );
+                FC_ASSERT( ser == VESTS_SYMBOL_SER, "invalid asset bits" );
+                sym.asset_num = STEEM_ASSET_NUM_VESTS;
+                break;
+              case BMT_SYMBOL_SER & 0xFFFFFFFF:
+                  s.read( ((char*) &ser)+4, 4 );
+                FC_ASSERT( ser == BMT_SYMBOL_SER, "invalid asset bits" );
+                sym.asset_num = STEEM_ASSET_NUM_BMT;
+                break;
+              case REP_SYMBOL_SER & 0xFFFFFFFF:
+                  s.read( ((char*) &ser)+4, 4 );
+                FC_ASSERT( ser == REP_SYMBOL_SER, "invalid asset bits" );
+                sym.asset_num = STEEM_ASSET_NUM_REP;
+                break;
+              default:
+                  sym.asset_num = uint32_t( ser );
+          }
+          sym.validate();
+      }
 
-    } // fc::raw
+   } // fc::raw
 
-    inline void to_variant( const bmchain::protocol::asset_symbol_type& sym, fc::variant& var )
-    {
-        try
-        {
-            std::vector< variant > v( 2 );
-            v[0] = sym.decimals();
-            v[1] = sym.to_nai_string();
-        } FC_CAPTURE_AND_RETHROW()
-    }
+   inline void to_variant( const bmchain::protocol::asset_symbol_type& sym, fc::variant& var )
+   {
+       try
+       {
+           std::vector< variant > v( 2 );
+           v[0] = sym.decimals();
+           v[1] = sym.to_nai_string();
+       } FC_CAPTURE_AND_RETHROW()
+   }
 
-    inline void from_variant( const fc::variant& var, bmchain::protocol::asset_symbol_type& sym )
-    {
-        try
-        {
-            auto v = var.as< std::vector< variant > >();
-            FC_ASSERT( v.size() == 2, "Expected tuple of length 2." );
+   inline void from_variant( const fc::variant& var, bmchain::protocol::asset_symbol_type& sym )
+   {
+       try
+       {
+           auto v = var.as< std::vector< variant > >();
+           FC_ASSERT( v.size() == 2, "Expected tuple of length 2." );
 
-            sym = bmchain::protocol::asset_symbol_type::from_nai_string( v[1].as< std::string >().c_str(), v[0].as< uint8_t >() );
-        } FC_CAPTURE_AND_RETHROW()
-    }
+           sym = bmchain::protocol::asset_symbol_type::from_nai_string( v[1].as< std::string >().c_str(), v[0].as< uint8_t >() );
+       } FC_CAPTURE_AND_RETHROW()
+   }
 
 } // fc
