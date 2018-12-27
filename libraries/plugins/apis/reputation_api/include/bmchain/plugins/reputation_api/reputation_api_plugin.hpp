9ecc1fa5c1ba75a6fbb88b56211a5142f7bd6626
igor <igor.soldatov@gmail.com>
2018-12-27 22:23:47 +0300

    get_account_reputations_return #

--- libraries/plugins/apis/reputation_api/include/bmchain/plugins/reputation_api/reputation_api_plugin.hpp
+++ libraries/plugins/apis/reputation_api/include/bmchain/plugins/reputation_api/reputation_api_plugin.hpp
@@ -1,9 +1,9 @@
 #pragma once
-#include <steem/plugins/json_rpc/utility.hpp>
-#include <steem/plugins/reputation/reputation_objects.hpp>
-#include <steem/plugins/database_api/database_api_objects.hpp>
+#include <bmchain/plugins/json_rpc/utility.hpp>
+#include <bmchain/plugins/reputation/reputation_objects.hpp>
+#include <bmchain/plugins/database_api/database_api_objects.hpp>
 
-#include <steem/protocol/types.hpp>
+#include <bmchain/protocol/types.hpp>
 
 #include <fc/optional.hpp>
 #include <fc/variant.hpp>
@@ -30,13 +30,29 @@ struct get_account_reputations_args
     uint32_t          limit = 1000;
 };
 
+struct get_account_reputations_return
+{
+    vector< account_reputation > reputations;
+};
+
+class reputation_api
+{
+public:
+    reputation_api();
+    ~reputation_api();
+
+    DECLARE_API(
+    (get_account_reputations)
+    )
+
+private:
+    std::unique_ptr< detail::reputation_api_impl > my;
+};
+
 } } } // bmchain::plugins::reputation
 
-FC_REFLECT( bmchain::plugins::reputation::account_reputation,
-            (account)(reputation) );
+FC_REFLECT( bmchain::plugins::reputation::account_reputation,(account)(reputation) );
 
-FC_REFLECT( bmchain::plugins::reputation::get_account_reputations_args,
-            (account_lower_bound)(limit) );
+FC_REFLECT( bmchain::plugins::reputation::get_account_reputations_args,(account_lower_bound)(limit) );
 
-FC_REFLECT( bmchain::plugins::reputation::get_account_reputations_return,
-            (reputations) );
+FC_REFLECT( bmchain::plugins::reputation::get_account_reputations_return,(reputations) );
