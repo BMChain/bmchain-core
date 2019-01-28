da28e8089340a7e643407250165522e1787ef8f4
igor <igor.soldatov@gmail.com>
2019-01-28 21:51:03 +0300

    Custom tokens js test #3

--- client_api/bmchain/test_custom_tokens.js
+++ client_api/bmchain/test_custom_tokens.js
@@ -1,125 +1,103 @@
-var aes = require('./steem/lib/auth/ecc/src/aes');
 const bmchain = require("./steem/lib");
 
-var custom_token_create = false;
-var get_custom_tokens = true;
-var custom_token_transfer = false;
-var custom_token_setup_emissions = false;
 
-function sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
-}
+(async () => {
+    // #1: custom_token_create
+    try {
+        const operations = [
+            ['custom_token_create',
+                {
+                    control_account: 'user001',
+                    current_supply: '1000000.000 NEW',
+                    custom_token_creation_fee: '1.000 BMT'
+                }
+            ]
+        ];
 
+        const tx = await bmchain.broadcast.sendAsync(
+            {operations, extensions: []},
+            {owner: '5JnWUyuk9jT3nW7x2pwFkz7qVLV7pgpKAXnM8dXAShwZqcXCYee'}
+        );
 
-if (custom_token_create) {
-    (async () => {
-        try {
-            const operations = [
-                ['custom_token_create',
-                    {
-                        control_account: 'user001',
-                        current_supply: '1000000.000 NEW',
-                        custom_token_creation_fee: '1.000 BMT'
-                    }
-                ]
-            ];
+        tx.should.have.properties([
+            'expiration',
+            'ref_block_num',
+            'ref_block_prefix',
+            'extensions',
+            'operations',
+            'signatures',
+        ]);
+    } catch (e) {
+        console.error(e);
+    }
 
-            const tx = await bmchain.broadcast.sendAsync(
-                {operations, extensions: []},
-                {owner: '5JnWUyuk9jT3nW7x2pwFkz7qVLV7pgpKAXnM8dXAShwZqcXCYee'}
-            );
+    // #2: get_custom_tokens
+    try {
+        const ret = await bmchain.api.getCustomTokens(10);
+        console.log(ret);
+    } catch (e) {
+        console.error(e);
+    }
 
-            tx.should.have.properties([
-                'expiration',
-                'ref_block_num',
-                'ref_block_prefix',
-                'extensions',
-                'operations',
-                'signatures',
-            ]);
-        } catch (e) {
-            console.error(e);
-        }
-    })();
-}
+    // #3: custom_token_transfer
+    try {
+        const operations = [
+            ['custom_token_transfer',
+                {
+                    from: 'user001',
+                    to: 'user002',
+                    amount: '1000.000 NEW'
+                }
+            ]
+        ];
 
+        const tx = await bmchain.broadcast.sendAsync(
+            {operations, extensions: []},
+            {owner: '5JnWUyuk9jT3nW7x2pwFkz7qVLV7pgpKAXnM8dXAShwZqcXCYee'}
+        );
 
-if (get_custom_tokens) {
-    (async () => {
-        try {
-            const ret = await bmchain.api.getCustomTokens(10);
-            console.log(ret);
-        } catch (e) {
-            console.error(e);
-        }
-    })();
-}
+        tx.should.have.properties([
+            'expiration',
+            'ref_block_num',
+            'ref_block_prefix',
+            'extensions',
+            'operations',
+            'signatures',
+        ]);
+    } catch (e) {
+        console.error(e);
+    }
 
+    // #3: custom_token_setup_emissions
+    try {
+        const operations = [
+            ['custom_token_setup_emissions',
+                {
+                    schedule_time: new Date('2019-01-22'),
+                    control_account: 'user001',
+                    symbol: bmchain.formatter.amount(0.000, 'NEW'),
+                    inflation_rate: 20,
+                    interval_seconds: 60,
+                    interval_count: 1
+                }
+            ]
+        ];
 
-if (custom_token_transfer) {
-    (async () => {
-        try {
-            const operations = [
-                ['custom_token_transfer',
-                    {
-                        from: 'user001',
-                        to: 'user002',
-                        amount: '1000.000 NEW'
-                    }
-                ]
-            ];
+        const tx = await bmchain.broadcast.sendAsync(
+            {operations, extensions: []},
+            {owner: '5JnWUyuk9jT3nW7x2pwFkz7qVLV7pgpKAXnM8dXAShwZqcXCYee'}
+        );
 
-            const tx = await bmchain.broadcast.sendAsync(
-                {operations, extensions: []},
-                {owner: '5JnWUyuk9jT3nW7x2pwFkz7qVLV7pgpKAXnM8dXAShwZqcXCYee'}
-            );
+        tx.should.have.properties([
+            'expiration',
+            'ref_block_num',
+            'ref_block_prefix',
+            'extensions',
+            'operations',
+            'signatures',
+        ]);
+    } catch (e) {
+        console.error(e);
+    }
 
-            tx.should.have.properties([
-                'expiration',
-                'ref_block_num',
-                'ref_block_prefix',
-                'extensions',
-                'operations',
-                'signatures',
-            ]);
-        } catch (e) {
-            console.error(e);
-        }
-    })();
-}
-
-
-if (custom_token_setup_emissions) {
-    (async () => {
-        try {
-            const operations = [
-                ['custom_token_setup_emissions',
-                    {
-                        schedule_time: new Date('2019-01-22'),
-                        control_account: 'user001',
-                        symbol: bmchain.formatter.amount(0.000, 'NEW'),
-                        inflation_rate: 20,
-                        interval_seconds: 60,
-                        interval_count: 1
-                    }
-                ]
-            ];
-
-            const tx = await bmchain.broadcast.sendAsync(
-                {operations, extensions: []},
-                {owner: '5JnWUyuk9jT3nW7x2pwFkz7qVLV7pgpKAXnM8dXAShwZqcXCYee'}
-            );
-
-            tx.should.have.properties([
-                'expiration',
-                'ref_block_num',
-                'ref_block_prefix',
-                'extensions',
-                'operations',
-                'signatures',
-            ]);
-        } catch (e) {
-            console.error(e);
-        }
-    })();
-}
+})();
